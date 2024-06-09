c4c96f17f746e311b3f02cedf582c4a8b4f80111
baltazar <baltamendez55@gmail.com>
2024-06-09 00:47:27 -0300

    Se agregan lineas de codigo

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,15 @@
 // Creamos una instancia del router de Express
+const express = require('express');
 const router = express.Router();
+
+
+// Datos ficticios de peliculas
+let movies = [
+    { id: 1, tiitle: "El secreto de sus ojos", director: "Juan José Campanella", year: 2009},
+    { id: 2, tiitle: "Relatos Salvajes", director: "Damián Szifron", year: 2014},
+    { id: 3, tiitle: "La historia oficial", director: "Luis Puenzo", year: 1985},
+    { id: 4, tiitle: "Nueve Reinas", director: "Fabián Bielinsky", year: 2000}
+];
 
 // Definimos una ruta para obtener todas las peliculas
 // Cuando se haga una solicituda GET a La ruta ('/movies/'), se ejecutará esta función
@@ -7,6 +17,13 @@ router.get('/', (req, res) => {
     // Envia una respuesta en formato JSON con un mensaje
     res.json({ mensaje: 'Hola desde la ruta de películas!'});
 
+});
+
+// Obtener una pelicula por ID
+router.get('/:id', (req, res) => {
+    const movie = movie.find(m => m.id === parseInt(req.params.id));
+    if (!movie) return res.status(404).send('Movie not found');
+    res.json(movie);
 });
 
 // Exporta el router para que pueda ser utilizado en otros archivos
